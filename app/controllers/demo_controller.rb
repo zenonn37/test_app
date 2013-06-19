2e6ab30902dc111faaa7e22166f4ce57a1ab5f70
chris crowd <zent29@gmail.com>
2013-06-19 16:59:12 -0400

    first commit test app lynda

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,10 @@
+class DemoController < ApplicationController
+  def index
+
+  end
+  def hello
+  	@array = [1,2,3,4,5]
+  	@id = params[:id].to_i
+  	@page = params[:page].to_i
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '536c80143e6a95b0d58cc68a9cee43596775cc0e53a308deca29f6a51a473abc33ba4c2b5c774c715aa84f8870da661a6a36bc4a522a5a5677543b1c5d311956'
