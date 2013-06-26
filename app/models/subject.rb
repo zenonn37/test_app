8d51602d0ac93160c565d4544ba1c8362a8bb105
chris crowd <zent29@gmail.com>
2013-06-26 08:46:06 -0400

    fixed

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,11 @@
 class Subject < ActiveRecord::Base
    attr_accessible :name, :position,:visible
+   
+   has_one :page
+   
+   scope :visible, where(:visible =>true)
+
+   scope :invisible, where(:visible => false)
+
+   scope :search, lambda{|query|where(["name Like ?",%#{query}%])}
 end
