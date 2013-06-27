d82a99831029571f162176d601230611317a36bd
chris crowd <zent29@gmail.com>
2013-06-27 17:21:11 -0400

    crud

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -11,4 +11,40 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
 
 	end
+
+	def new
+		@subject = Subject.new
+	end
+
+	def create
+		@subject = Subject.new(params[:subject])
+
+		if @subject.save
+
+			redirect_to(:action=>'list')
+
+		else
+
+			render('new')
+		end
+	end
+
+	def edit
+		@subject = Subject.find(params[:id])
+	end
+
+	def update
+		@subject = Subject.find(params[:id])
+
+		if @subject.update_attributes(params[:subject])
+
+			redirect_to(:action=>'show',:id=>@subject.id)
+		else
+			render('edit')
+		end
+	end
+
+	def delete
+		@subject = Subject.find(params[:id])
+	end
 end
